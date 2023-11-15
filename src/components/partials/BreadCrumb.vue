5fc839b4d1d64a9d2c4c36dd7385a01d38ac55cc
nebstarmalala <nebstartrizzle@gmail.com>
2023-11-15 19:17:04 +0300

    add partial components

---
+++ src/components/partials/BreadCrumb.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-breadcrumbs :items="items">
+      <template v-slot:item="{ item }">
+        <v-breadcrumbs-item
+          :to="item.to"
+          class="text-subtitle-2 crumb-item"
+          :disabled="item.disabled"
+          exact
+        >
+          {{ item.text }}
+        </v-breadcrumbs-item>
+      </template>
+      <template v-slot:divider>
+        <v-icon>{{ icons.mdiChevronRight }}</v-icon>
+      </template>
+    </v-breadcrumbs>
+  </template>
+  
+  <script>
+  import {
+    mdiChevronRight,
+  } from '@mdi/js'
+  export default {
+    props: {
+      items: {
+        type: Array,
+        default: null,
+      },
+    },
+    data: () => ({
+      icons: {
+        mdiChevronRight,
+      },
+    }),
+  }
+  </script>
+  
+  <style>
+  </style>
---
+++ src/components/partials/RefreshButton.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-btn
+      :disabled="loading"
+      class="text-none"
+      @click="$emit('click')"
+    >
+      Refresh
+      <v-icon
+        right
+        :class="{ rotating: loading }"
+      >
+        {{ icons.mdiCached }}
+      </v-icon>
+    </v-btn>
+  </template>
+  
+  <script>import {
+    mdiCached,
+  } from '@mdi/js'
+  export default {
+    props: {
+      loading: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    data() {
+      return {
+        icons: {
+          mdiCached,
+        },
+      }
+    },
+  }
+  </script>
+  
+  <style>
+  .rotating {
+    -webkit-animation: rotation 2s infinite linear;
+    animation: rotation 2s infinite linear;
+  }
+  @keyframes rotation{
+    from {
+  -webkit-transform: rotate(0deg);
+  }
+  to {
+  -webkit-transform: rotate(359deg);
+  }
+  }
+  @-webkit-keyframes rotation {
+  from {
+  -webkit-transform: rotate(0deg);
+  }
+  to {
+  -webkit-transform: rotate(359deg);
+  }
+  }
+  </style>
