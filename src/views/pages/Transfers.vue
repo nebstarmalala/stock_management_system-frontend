b0d13200ac4daf3bb734363ae7bd33f876475422
nebstarmalala <nebstartrizzle@gmail.com>
2023-11-19 22:40:50 +0300

    create view table for transfers

---
+++ src/views/pages/Transfers.vue
@@ -0,0 +1,390 @@
+<template>
+    <div class="transfers">
+      <v-row>
+        <v-col
+          sm="12"
+          md="6"
+        >
+          <breadcrumb :items="breadcrumbs" />
+        </v-col>
+        <v-col
+          sm="12"
+          md="6"
+          class="text-md-right"
+        >
+          <v-btn
+            color="primary"
+            class="mb-5"
+            @click="showAddTransferDialog = true"
+          >
+            <v-icon
+              left
+              size="22"
+            >
+              {{ icons.mdiPlus }}
+            </v-icon>
+            Add Transfer
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-expand-transition>
+        <v-card>
+          <v-card-title>
+            <v-row
+              justify="center"
+              class="mb-0"
+            >
+              <v-col
+                md="4"
+                sm="12"
+              >
+                <refresh-button
+                  :loading="isTransfersRefreshing"
+                  @click="refreshTransfers()"
+                />
+              </v-col>
+              <v-col
+                md="6"
+                sm="12"
+                class="text-center"
+              >
+                <search-input
+                  v-model="search"
+                  :filters="searchFilters"
+                  :active-filter="searchColumn"
+                  @filter-change="onSearchFilterChange"
+                />
+              </v-col>
+              <v-spacer class="d-none d-md-inline" />
+            </v-row>
+          </v-card-title>
+          <v-expand-transition>
+            <v-row
+              v-if="selected.length > 0"
+              class="mb-2"
+            >
+              <v-col class="text-md-right">
+                <v-btn
+                  color="error"
+                  class="ma-2"
+                  @click="showConfirmBulkDeleteDialog = true"
+                >
+                  <v-icon
+                    left
+                    size="22"
+                  >
+                    {{ icons.mdiDelete }}
+                  </v-icon>
+                  Delete Selected
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-expand-transition>
+          <v-data-table
+            v-model="selected"
+            :headers="headers"
+            :items="transfers"
+            :loading="isTransfersLoading"
+            :server-items-length="pagination.totalItems"
+            :options.sync="options"
+            :footer-props="footerProps"
+            show-select
+            class="clickable-data-table"
+            @click:row="transferDetails"
+          >
+            <template v-slot:item.category="{ item }">
+              {{ item.category.name }}
+            </template>
+            <template
+              v-slot:item.actions="{ item }"
+            >
+              <v-tooltip
+                bottom
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    ma-4
+                    v-bind="attrs"
+                    text
+                    icon
+                    @click.stop="editTransfer(item)"
+                    v-on="on"
+                  >
+                    <v-icon
+                      v-bind="attrs"
+                      v-on="on"
+                    >
+                      {{ icons.mdiPencil }}
+                    </v-icon>
+                  </v-btn>
+                </template>
+                <span>Edit</span>
+              </v-tooltip>
+              <v-tooltip
+                bottom
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    ma-4
+                    v-bind="attrs"
+                    text
+                    icon
+                    @click.stop="showDeleteTransferDialog(item.id)"
+                    v-on="on"
+                  >
+                    <v-icon
+                      v-bind="attrs"
+                      v-on="on"
+                    >
+                      {{ icons.mdiDelete }}
+                    </v-icon>
+                  </v-btn>
+                </template>
+                <span>Delete</span>
+              </v-tooltip>
+            </template>
+          </v-data-table>
+        </v-card>
+      </v-expand-transition>
+      <add-transfer-dialog
+        :show-dialog="showAddTransferDialog"
+        :transfer="transfer"
+        @close="
+          showAddTransferDialog = false
+          transfer = {}
+        "
+        @transfer-added="onTransferAdded"
+        @transfer-updated="onTransferUpdated"
+      />
+      <confirm-dialog
+        :show-dialog="showConfirmDeleteDialog"
+        :is-agree-button-loading="isConfirmDeleteDialogButtonLoading"
+        :agree-text="'Delete'"
+        @cancel="showConfirmDeleteDialog = false"
+        @agree="deleteTransfer()"
+      />
+      <confirm-dialog
+        :show-dialog="showConfirmBulkDeleteDialog"
+        :message="'Are you sure you want to delete ' + selected.length + ' records?'"
+        :is-agree-button-loading="isConfirmBulkDeleteDialogButtonLoading"
+        :agree-text="'Delete'"
+        @cancel="showConfirmBulkDeleteDialog = false"
+        @agree="deleteSelectedTransfers()"
+      />
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  import _ from 'lodash'
+  import {
+    mdiMagnify, mdiPencil, mdiDelete, mdiPlus,
+  } from '@mdi/js'
+  import AddTransferDialog from '@/components/dialogs/AddTransferDialog.vue'
+  import ConfirmDialog from '@/components/dialogs/ConfirmDialog.vue'
+  import BreadCrumb from '@/components/partials/BreadCrumb.vue'
+  import SearchInput from '@/components/partials/SearchInput.vue'
+  import RefreshButton from '@/components/partials/RefreshButton.vue'
+  
+  export default {
+    components: {
+      AddTransferDialog,
+      ConfirmDialog,
+      breadcrumb: BreadCrumb,
+      SearchInput,
+      RefreshButton,
+    },
+    data() {
+      return {
+        showAddTransferDialog: false,
+        showConfirmDeleteDialog: false,
+        showConfirmBulkDeleteDialog: false,
+        isConfirmDeleteDialogButtonLoading: false,
+        isConfirmBulkDeleteDialogButtonLoading: false,
+        isTransfersRefreshing: false,
+        toDelete: null,
+        sortBy: ['name'],
+        sortDesc: [true],
+        search: '',
+        pagination: {
+          page: 1,
+          pageCount: 1,
+          itemsPerPage: 10,
+          totalItems: 0,
+        },
+        footerProps: {
+          itemsPerPageOptions: [5, 10, 25, 50, 100],
+        },
+        selected: [],
+        options: {},
+        headers: [
+          {
+            text: 'Product',
+            value: 'product.name',
+          },
+          {
+            text: 'Source',
+            value: 'source.name',
+          },
+          {
+            text: 'Destination',
+            value: 'destination.name',
+          },
+          {
+            text: 'Quantity',
+            value: 'quantity',
+          },
+          {
+            text: 'Status',
+            value: 'status',
+          },
+          { text: 'Actions', value: 'actions', sortable: false },
+        ],
+        searchColumn: 'title',
+        searchFilters: [
+          {
+            text: 'Name',
+            value: 'name',
+          },
+          {
+            text: 'UPC Code',
+            value: 'upc_code',
+          },
+          {
+            text: 'Price',
+            value: 'price',
+          },
+        ],
+        icons: {
+          mdiMagnify,
+          mdiPencil,
+          mdiDelete,
+          mdiPlus,
+        },
+        transfer: {},
+        transfers: [],
+        isTransfersLoading: true,
+        transfersUrl: 'stock_transfers',
+        breadcrumbs: [
+          {
+            text: 'Dashboard',
+            disabled: false,
+            to: { name: 'dashboard' },
+          },
+          {
+            text: 'Transfers',
+            disabled: false,
+            to: { name: 'transfers' },
+          },
+        ],
+      }
+    },
+    computed: {
+      refreshTable() {
+        return `${this.transfersUrl}|${this.search}|${this.sortBy}|${this.sortDesc}`
+      },
+    },
+    watch: {
+      options: {
+        handler() {
+          this.getTransfers()
+        },
+        deep: true,
+      },
+      refreshTable() {
+        this.getTransfers()
+      },
+      searchColumn() {
+        if (this.search !== '') {
+          this.getTransfers()
+        }
+      },
+    },
+    mounted() {
+      this.getTransfers()
+    },
+    methods: {
+      getTransfers: _.debounce(function () {
+        this.isTransfersLoading = true
+        const {
+          sortBy, sortDesc, page, itemsPerPage,
+        } = this.options
+        const sortOrder = sortDesc[0] ? 'desc' : 'asc'
+        axios
+          .get(
+            `${this.transfersUrl}?paginate=true&sortBy=${sortBy[0]}&sortDirection=${sortOrder}&search=${this.search}&searchColumn=${this.searchColumn}&page=${page}&perPage=${itemsPerPage}`,
+          )
+          .then(response => {
+            this.transfers = response.data.data
+            this.pagination.page = response.data.current_page
+            this.pagination.pageCount = response.data.last_page
+            this.pagination.itemsPerPage = response.data.per_page
+            this.pagination.totalItems = response.data.total
+          })
+          .catch(error => {
+            this.$toast.error(error.response.data.message)
+          }).finally(() => {
+            this.isTransfersLoading = false
+            this.isTransfersRefreshing = false
+          })
+      }, 500),
+      refreshTransfers() {
+        this.isTransfersRefreshing = true
+        this.getTransfers()
+      },
+      editTransfer(transfer) {
+        this.transfer = transfer
+        this.showAddTransferDialog = true
+      },
+      showDeleteTransferDialog(id) {
+        this.showConfirmDeleteDialog = true
+        this.toDelete = id
+      },
+      deleteSelectedTransfers() {
+        this.isConfirmBulkDeleteDialogButtonLoading = true
+        const selectedIds = this.selected.map(transfer => transfer.id)
+        axios
+          .delete('bulk-delete/stock_transfers', { data: { ids: selectedIds } })
+          .then(() => {
+            this.selected = []
+            this.getTransfers()
+          })
+          .catch(error => {
+            this.$toast.error(error.response.data.message)
+          }).finally(() => {
+            this.showConfirmBulkDeleteDialog = false
+            this.isConfirmBulkDeleteDialogButtonLoading = false
+          })
+      },
+      deleteTransfer() {
+        this.isConfirmDeleteDialogButtonLoading = true
+        axios
+          .delete(`stock_transfers/${this.toDelete}`)
+          .then(() => {
+            this.refreshTransfers()
+            this.$toast.success('Transfer deleted successfully')
+          }).catch(error => {
+            this.$toast.error(error.response.data.message)
+          }).finally(() => {
+            this.showConfirmDeleteDialog = false
+            this.isConfirmDeleteDialogButtonLoading = false
+          })
+      },
+      onTransferAdded() {
+        this.refreshTransfers()
+      },
+      onTransferUpdated() {
+        this.refreshTransfers()
+      },
+      onPageChange() {
+        this.getTransfers()
+      },
+      onSearchFilterChange(filter) {
+        this.searchColumn = filter
+      },
+      // transferDetails(transfer) {
+      //   this.$router.push({ name: 'transfer-details', params: { id: transfer.id } })
+      // },
+    },
+  }
+  </script>
