67341342f20b0afa0e839a02da5ad762fa735f82
nebstarmalala <nebstartrizzle@gmail.com>
2023-11-20 22:44:01 +0300

    create orders view

---
+++ src/views/pages/Orders.vue
@@ -0,0 +1,386 @@
+<template>
+    <div class="orders">
+      <v-row>
+        <v-col
+          sm="12"
+          md="6"
+        >
+          <breadcrumb :items="breadcrumbs" />
+        </v-col>
+        <v-col
+          sm="12"
+          md="6"
+          class="text-md-right"
+        >
+          <v-btn
+            color="primary"
+            class="mb-5"
+            @click="showAddOrderDialog = true"
+          >
+            <v-icon
+              left
+              size="22"
+            >
+              {{ icons.mdiPlus }}
+            </v-icon>
+            Add Order
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-expand-transition>
+        <v-card>
+          <v-card-title>
+            <v-row
+              justify="center"
+              class="mb-0"
+            >
+              <v-col
+                md="4"
+                sm="12"
+              >
+                <refresh-button
+                  :loading="isOrdersRefreshing"
+                  @click="refreshOrders()"
+                />
+              </v-col>
+              <v-col
+                md="6"
+                sm="12"
+                class="text-center"
+              >
+                <search-input
+                  v-model="search"
+                  :filters="searchFilters"
+                  :active-filter="searchColumn"
+                  @filter-change="onSearchFilterChange"
+                />
+              </v-col>
+              <v-spacer class="d-none d-md-inline" />
+            </v-row>
+          </v-card-title>
+          <v-expand-transition>
+            <v-row
+              v-if="selected.length > 0"
+              class="mb-2"
+            >
+              <v-col class="text-md-right">
+                <v-btn
+                  color="error"
+                  class="ma-2"
+                  @click="showConfirmBulkDeleteDialog = true"
+                >
+                  <v-icon
+                    left
+                    size="22"
+                  >
+                    {{ icons.mdiDelete }}
+                  </v-icon>
+                  Delete Selected
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-expand-transition>
+          <v-data-table
+            v-model="selected"
+            :headers="headers"
+            :items="orders"
+            :loading="isOrdersLoading"
+            :server-items-length="pagination.totalItems"
+            :options.sync="options"
+            :footer-props="footerProps"
+            show-select
+            class="clickable-data-table"
+            @click:row="orderDetails"
+          >
+            <template v-slot:item.category="{ item }">
+              {{ item.category.name }}
+            </template>
+            <template
+              v-slot:item.actions="{ item }"
+            >
+              <v-tooltip
+                bottom
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    ma-4
+                    v-bind="attrs"
+                    text
+                    icon
+                    @click.stop="editOrder(item)"
+                    v-on="on"
+                  >
+                    <v-icon
+                      v-bind="attrs"
+                      v-on="on"
+                    >
+                      {{ icons.mdiPencil }}
+                    </v-icon>
+                  </v-btn>
+                </template>
+                <span>Edit</span>
+              </v-tooltip>
+              <v-tooltip
+                bottom
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    ma-4
+                    v-bind="attrs"
+                    text
+                    icon
+                    @click.stop="showDeleteOrderDialog(item.id)"
+                    v-on="on"
+                  >
+                    <v-icon
+                      v-bind="attrs"
+                      v-on="on"
+                    >
+                      {{ icons.mdiDelete }}
+                    </v-icon>
+                  </v-btn>
+                </template>
+                <span>Delete</span>
+              </v-tooltip>
+            </template>
+          </v-data-table>
+        </v-card>
+      </v-expand-transition>
+      <add-order-dialog
+        :show-dialog="showAddOrderDialog"
+        :order="order"
+        @close="
+          showAddOrderDialog = false
+          order = {}
+        "
+        @order-added="onOrderAdded"
+        @order-updated="onOrderUpdated"
+      />
+      <confirm-dialog
+        :show-dialog="showConfirmDeleteDialog"
+        :is-agree-button-loading="isConfirmDeleteDialogButtonLoading"
+        :agree-text="'Delete'"
+        @cancel="showConfirmDeleteDialog = false"
+        @agree="deleteOrder()"
+      />
+      <confirm-dialog
+        :show-dialog="showConfirmBulkDeleteDialog"
+        :message="'Are you sure you want to delete ' + selected.length + ' records?'"
+        :is-agree-button-loading="isConfirmBulkDeleteDialogButtonLoading"
+        :agree-text="'Delete'"
+        @cancel="showConfirmBulkDeleteDialog = false"
+        @agree="deleteSelectedOrders()"
+      />
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  import _ from 'lodash'
+  import {
+    mdiMagnify, mdiPencil, mdiDelete, mdiPlus,
+  } from '@mdi/js'
+  import AddOrderDialog from '@/components/dialogs/AddOrderDialog.vue'
+  import ConfirmDialog from '@/components/dialogs/ConfirmDialog.vue'
+  import BreadCrumb from '@/components/partials/BreadCrumb.vue'
+  import SearchInput from '@/components/partials/SearchInput.vue'
+  import RefreshButton from '@/components/partials/RefreshButton.vue'
+  
+  export default {
+    components: {
+      AddOrderDialog,
+      ConfirmDialog,
+      breadcrumb: BreadCrumb,
+      SearchInput,
+      RefreshButton,
+    },
+    data() {
+      return {
+        showAddOrderDialog: false,
+        showConfirmDeleteDialog: false,
+        showConfirmBulkDeleteDialog: false,
+        isConfirmDeleteDialogButtonLoading: false,
+        isConfirmBulkDeleteDialogButtonLoading: false,
+        isOrdersRefreshing: false,
+        toDelete: null,
+        sortBy: ['name'],
+        sortDesc: [true],
+        search: '',
+        pagination: {
+          page: 1,
+          pageCount: 1,
+          itemsPerPage: 10,
+          totalItems: 0,
+        },
+        footerProps: {
+          itemsPerPageOptions: [5, 10, 25, 50, 100],
+        },
+        selected: [],
+        options: {},
+        headers: [
+          {
+            text: 'Supplier Name',
+            value: 'supplier.name',
+          },
+          {
+            text: 'Supplier Phone',
+            value: 'supplier.phone',
+          },
+          {
+            text: 'Product Name',
+            value: 'product.name',
+          },
+          {
+            text: 'Quantity',
+            value: 'quantity',
+          },
+          { text: 'Actions', value: 'actions', sortable: false },
+        ],
+        searchColumn: 'title',
+        searchFilters: [
+          {
+            text: 'Name',
+            value: 'name',
+          },
+          {
+            text: 'UPC Code',
+            value: 'upc_code',
+          },
+          {
+            text: 'Price',
+            value: 'price',
+          },
+        ],
+        icons: {
+          mdiMagnify,
+          mdiPencil,
+          mdiDelete,
+          mdiPlus,
+        },
+        order: {},
+        orders: [],
+        isOrdersLoading: true,
+        ordersUrl: 'purchase_orders',
+        breadcrumbs: [
+          {
+            text: 'Dashboard',
+            disabled: false,
+            to: { name: 'dashboard' },
+          },
+          {
+            text: 'orders',
+            disabled: false,
+            to: { name: 'orders' },
+          },
+        ],
+      }
+    },
+    computed: {
+      refreshTable() {
+        return `${this.ordersUrl}|${this.search}|${this.sortBy}|${this.sortDesc}`
+      },
+    },
+    watch: {
+      options: {
+        handler() {
+          this.getOrders()
+        },
+        deep: true,
+      },
+      refreshTable() {
+        this.getOrders()
+      },
+      searchColumn() {
+        if (this.search !== '') {
+          this.getOrders()
+        }
+      },
+    },
+    mounted() {
+      this.getOrders()
+    },
+    methods: {
+      getOrders: _.debounce(function () {
+        this.isOrdersLoading = true
+        const {
+          sortBy, sortDesc, page, itemsPerPage,
+        } = this.options
+        const sortOrder = sortDesc[0] ? 'desc' : 'asc'
+        axios
+          .get(
+            `${this.ordersUrl}?paginate=true&sortBy=${sortBy[0]}&sortDirection=${sortOrder}&search=${this.search}&searchColumn=${this.searchColumn}&page=${page}&perPage=${itemsPerPage}`,
+          )
+          .then(response => {
+            this.orders = response.data.data
+            this.pagination.page = response.data.current_page
+            this.pagination.pageCount = response.data.last_page
+            this.pagination.itemsPerPage = response.data.per_page
+            this.pagination.totalItems = response.data.total
+          })
+          .catch(error => {
+            this.$toast.error(error.response.data.message)
+          }).finally(() => {
+            this.isOrdersLoading = false
+            this.isOrdersRefreshing = false
+          })
+      }, 500),
+      refreshOrders() {
+        this.isOrdersRefreshing = true
+        this.getOrders()
+      },
+      editOrder(order) {
+        this.order = order
+        this.showAddOrderDialog = true
+      },
+      showDeleteOrderDialog(id) {
+        this.showConfirmDeleteDialog = true
+        this.toDelete = id
+      },
+      deleteSelectedOrders() {
+        this.isConfirmBulkDeleteDialogButtonLoading = true
+        const selectedIds = this.selected.map(order => order.id)
+        axios
+          .delete('bulk-delete/purchase_orders', { data: { ids: selectedIds } })
+          .then(() => {
+            this.selected = []
+            this.getOrders()
+          })
+          .catch(error => {
+            this.$toast.error(error.response.data.message)
+          }).finally(() => {
+            this.showConfirmBulkDeleteDialog = false
+            this.isConfirmBulkDeleteDialogButtonLoading = false
+          })
+      },
+      deleteOrder() {
+        this.isConfirmDeleteDialogButtonLoading = true
+        axios
+          .delete(`purchase_orders/${this.toDelete}`)
+          .then(() => {
+            this.refreshOrders()
+            this.$toast.success('Order deleted successfully')
+          }).catch(error => {
+            this.$toast.error(error.response.data.message)
+          }).finally(() => {
+            this.showConfirmDeleteDialog = false
+            this.isConfirmDeleteDialogButtonLoading = false
+          })
+      },
+      onOrderAdded() {
+        this.refreshOrders()
+      },
+      onOrderUpdated() {
+        this.refreshOrders()
+      },
+      onPageChange() {
+        this.getOrders()
+      },
+      onSearchFilterChange(filter) {
+        this.searchColumn = filter
+      },
+      orderDetails(order) {
+        this.$router.push({ name: 'order-details', params: { id: order.id } })
+      },
+    },
+  }
+  </script>
