7062f81c5be9e23fb1d6bdb6abada852847f7ea5
nebstarmalala <nebstartrizzle@gmail.com>
2023-12-19 21:41:51 +0300

    add router for shop assignment view

--- src/router/index.js
+++ src/router/index.js
@@ -98,6 +98,14 @@ const routes = [
     },
   },
   {
+    path: '/assignments',
+    name: 'assignments',
+    component: () => import('@/views/pages/Assignments.vue'),
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
     path: '/account-settings',
     name: 'pages-account-settings',
     component: () => import('@/views/pages/account-settings/AccountSettings.vue'),
